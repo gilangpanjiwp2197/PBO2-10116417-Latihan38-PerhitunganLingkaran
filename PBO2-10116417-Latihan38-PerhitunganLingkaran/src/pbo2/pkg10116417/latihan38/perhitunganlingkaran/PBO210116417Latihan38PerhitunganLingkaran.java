382c18833489ec8634630e052cf769567b6c47b0
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-15 10:58:27 +0700

    Add files via upload

---
+++ PBO2-10116417-Latihan38-PerhitunganLingkaran/src/pbo2/pkg10116417/latihan38/perhitunganlingkaran/PBO210116417Latihan38PerhitunganLingkaran.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan38.perhitunganlingkaran;
+
+/**
+ *
+ * @author
+ *  NAMA        : GILANG PANJI WANENG PATI
+ *  KELAS       : PBO2
+ *  NIM         : 10116417
+ *  DESKRIPSI   : BERISI TENTANG PROGRAM MENAMPILKAN PERHITUNGAN LINGKARAN  
+ */
+public class PBO210116417Latihan38PerhitunganLingkaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Lingkaran ling = new Lingkaran();
+
+        System.out.println("=======Perhitungan Lingkaran=======");
+        ling.validasiInput();
+        ling.hasilPerhitungan(ling.diameter);
+    }
+    
+}
